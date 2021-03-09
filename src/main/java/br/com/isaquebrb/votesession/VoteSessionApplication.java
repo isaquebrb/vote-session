a28cb33a2945c422b848505272a5c319d5a32b89
Isaque Ribeiro <isaque.ribeiro.bernardes@gmail.com>
2021-03-08 22:06:42 -0300

    initializing project

---
+++ src/main/java/br/com/isaquebrb/votesession/VoteSessionApplication.java
@@ -0,0 +1,13 @@
+package br.com.isaquebrb.votesession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteSessionApplication.class, args);
+	}
+
+}
